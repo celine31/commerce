e10b8138c9b8e8be487b3e79030da4e9d8329d2e
celine31 <dadou20231@gmail.com>
2018-01-18 16:37:08 +0100

    td3 fini

--- commerce/td3/index.php
+++ commerce/td3/index.php
@@ -5,18 +5,17 @@ const OPT = [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8mb4'",
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 try {
     $pdo = new PDO(DSN, LOG, '', OPT);
-   
 } catch (PDOException $e) {
-     exit("Erreur : {$e->getMessage()}");
+    exit("Erreur : {$e->getMessage()}");
 }
-/*$req = "DELETE FROM produit WHERE id_produit=3";
-echo $pdo->exec($req);*/
+/* $req = "DELETE FROM produit WHERE id_produit=3";
+  echo $pdo->exec($req); */
 ?>
 <?php
-        $req="SELECT * FROM produit ORDER BY nom ";
-        $jeu=$pdo->query($req);
-        $jeu->setFetchMode(PDO::FETCH_OBJ);
-        $tab=$jeu->fetchAll();
+$req = "SELECT * FROM produit ORDER BY nom ";
+$jeu = $pdo->query($req);
+$jeu->setFetchMode(PDO::FETCH_OBJ);
+$tab = $jeu->fetchAll();
 ?>        
 <!DOCTYPE html>
 <html>
@@ -26,23 +25,21 @@ echo $pdo->exec($req);*/
         <link rel = "stylesheet" href="css/commerce.css"/>
     </head>
     <body>
-         <table>
-                       
-           <tr>
-               <th> nom </th>
-               <th> Référence </th>
-               <th> Prix </th>
-           </tr>
-           <?php foreach($tab as $prod){?>
-          <tr>
-               <td><?=$prod->nom?>
-               </td>
-               <td><?=$prod->ref?>
-               </td>
-               <td><?=$prod->prix?>
-               </td>
-           <?php }?>
-            </table>
-      
-    </body>
+        <div id ="conteneur"> 
+        <?php foreach ($tab as $prod) { 
+            $file="img/prod_{$prod->id_produit}_v.jpg"
+            ?>
+        <div class="vignette">
+            <?php if (file_exists($file)){?>
+            <img src = "img/prod_<?= $prod->id_produit?>_v.jpg" alt= "image"/>
+            <?php }
+            else echo "vignette indisponible"?>   
+            <h1><?= $prod->nom ?></h1>
+            <p> <?= $prod->prix ?></p>
+        </div>
+          
+    <?php } ?>
+        </div>
+
+</body>
 </html>
