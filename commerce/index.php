6b86eee082ed65175e56144f28cd26d847d3c218
celine31 <dadou20231@gmail.com>
2018-01-18 11:40:15 +0100

    création constante de PDO index.php

--- commerce/index.php
+++ commerce/index.php
@@ -1,5 +1,10 @@
-<?php $pdo = new PDO('mysql:dbname=commerce;host=localhost;charset=utf8mb4','root','',[PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8mb4'",
-PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+<?php 
+const DSN = 'mysql:dbname=commerce;host=localhost;charset=utf8mb4';
+const LOG ='root';
+const OPT= [PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8mb4'",
+PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
+
+$pdo = new PDO(DSN,LOG,'',OPT);
 ?>
 <!DOCTYPE html>
 <html>
