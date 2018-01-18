477be0054495c7776aee6285634609c894b6ade9
celine31 <dadou20231@gmail.com>
2018-01-18 13:28:00 +0100

    ajout de catch ans try

--- commerce/index.php
+++ commerce/index.php
@@ -3,8 +3,12 @@ const DSN = 'mysql:dbname=commerce;host=localhost;charset=utf8mb4';
 const LOG ='root';
 const OPT= [PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8mb4'",
 PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION];
-
+try{
 $pdo = new PDO(DSN,LOG,'',OPT);
+}
+catch(PDOException $e){
+    
+}
 ?>
 <!DOCTYPE html>
 <html>
