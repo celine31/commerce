599adfe058bc0cd7a30959d8e94fb4a39a293460
celine31 <dadou20231@gmail.com>
2018-01-18 11:34:16 +0100

    creation index.php

---
+++ commerce/index.php
@@ -0,0 +1,15 @@
+<?php $pdo = new PDO('mysql:dbname=commerce;host=localhost;charset=utf8mb4','root','',[PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8mb4'",
+PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>commerce</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
