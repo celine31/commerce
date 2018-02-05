a37d9ec97f2041c623ec1cd7f97e2e31a4153fa3
celine31 <dadou20231@gmail.com>
2018-02-05 09:03:34 +0100

    début td12 resimensionnement d'image

---
+++ commerce/td12/class/Categorie.php
@@ -0,0 +1,34 @@
+<?php
+
+class Categorie {
+
+	public $id_categorie;
+	public $nom;
+
+	public function __construct($id_categorie = null, $nom = null) {
+		$this->id_categorie = $id_categorie;
+		$this->nom = $nom;
+	}
+	
+	public function getTabProduit(){
+		global $db;
+		$req = "SELECT * FROM produit WHERE id_categorie = {$this->id_categorie} ORDER BY nom";
+		$jeu = $db->query($req);
+		$jeu->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Produit');
+		return $jeu->fetchAll();
+	}
+
+        public function existe(){
+                if ($id_categorie==$this->id_categorie)
+                    return true; 
+        }
+        
+	public static function tous() {
+		global $db;
+		$req = "SELECT * FROM categorie ORDER BY nom";
+		$jeu = $db->query($req);
+		$jeu->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Categorie');
+		return $jeu->fetchAll();
+	}
+
+}
---
+++ commerce/td12/supprimer.php
@@ -0,0 +1,29 @@
+<?php
+
+/* ou
+  require_once 'inc/cfg.php';
+  header('Content-Type:application/json');
+  $reponse = false;
+  $data = json_decode(filter_input(INPUT_POST, 'data'));
+  if (property_exists($data, 'id_produit') && is_int($data->id_produit)) {
+  $req = "DELETE FROM produit WHERE id_produit={$data->id_produit}";
+  $reponse = $db->exec($req);
+  }
+  echo json_encode($reponse);
+ */
+/*2ème version pour garantir la stabilité pour de futurs modifications et 
+éviter des messages d'erreurs en cas de piratages (si une chaine de caractères est passée)*/
+require_once 'inc/cfg.php';
+header('Content-Type:application/json');
+$reponse = false;
+$data = json_decode(filter_input(INPUT_POST, 'data'));
+$opt=['options'=>['min_range'=>1]];
+if (property_exists($data, 'id_produit')) {
+    $id_produit = filter_var($data->id_produit, FILTER_VALIDATE_INT, $opt);
+    if ($id_produit) {
+        $req = "DELETE FROM produit WHERE id_produit={$data->id_produit}";
+        $reponse = $db->exec($req);
+    }
+}
+echo json_encode($reponse);//garantie de recevoir un message d'erreur même en cas de false 
+
