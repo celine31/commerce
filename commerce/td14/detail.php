0abb1c549f9d99aa50d5b1f7ad8b4c2890b094e5
celine31 <dadou20231@gmail.com>
2018-02-08 16:27:07 +0100

    création de la classe Connexion

---
+++ commerce/td14/detail.php
@@ -0,0 +1,42 @@
+<?php
+require_once 'inc/cfg.php';
+require_once 'class/Produit.php';
+require_once 'class/Categorie.php';
+$opt = ['options' => ['min_range' => 1]];
+$id_produit = filter_input(INPUT_GET, 'id_produit', FILTER_VALIDATE_INT, $opt);
+if (!$id_produit) {
+	header('Location:indispo.php');
+	exit("Produit indisponible");
+}
+$req = "SELECT * FROM produit WHERE id_produit={$id_produit}";
+$jeu = $db->query($req);
+$jeu->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Produit');
+$produit = $jeu->fetch();
+if (!$produit) {
+	header('Location:indispo.php');
+	exit("Produit indisponible");
+}
+$idImg = file_exists("../img/prod_{$produit->id_produit}_p.jpg") ? $produit->id_produit : 0;
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title><?= $produit->nom ?></title>
+		<link rel="stylesheet" type="text/css" href="css/commerce.css"/>
+	</head>
+	<body>
+		<header></header>
+		<div id="container">
+			<div class="categorie"><a href="javascript:history.go(-1)"><?= $produit->getCategorie()->nom ?></a> &gt; <?= $produit->nom ?></div>
+			<div id="detailProduit">
+				<img src="../img/prod_<?= $idImg ?>_p.jpg?alea=<?= rand() ?>" alt=""/>
+				<div>
+					<div class="prix"><?= $produit->prix ?></div>
+					<div class="ref">Référence<br/>
+						<?= $produit->ref ?></div>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
